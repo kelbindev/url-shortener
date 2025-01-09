b6fd63e30e616e0af523729663b2dac699d98fdd
Kelbin <kelbin@shimano.com.sg>
2025-01-09 10:48:18 +0800

    initial commit

---
+++ Entities/ShortUrl.cs
@@ -0,0 +1,7 @@
+﻿namespace UrlShortener.Entities;
+
+internal class ShortUrl(Uri url)
+{
+    public int Id { get; protected set; }
+    public string Url { get; protected set; } = url.ToString();
+}
